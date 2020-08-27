a33abed29a6f9704f72b561486c357bb0a24b1c8
Daniel Yeboah <dannysark@gmail.com>
2020-08-27 21:08:12 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import HomePage from './homepage.component';
+
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className='App'>
+        
+        <HomePage />
+        
+      </div>
+    );
+  }
+}
+
+export default App;
