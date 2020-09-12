35245ab49c89f1cabae509595ca3d1f4b9c24e0a
DanielSarkwa <dannysark16@gmail.com>
2020-09-12 08:07:26 -0800

    completed adding the cart selectors

---
+++ src/redux/cart/cart.selectors.js
@@ -0,0 +1,20 @@
+import { createSelector } from 'reselect';
+
+// input selector -- selects reducers from the redux state
+const selectCart = state => state.cart;
+
+// output selectors onUpdate from cart reducer
+export const selectCartItems = createSelector(
+    [selectCart],
+    // returns the value(list of cart items) from the selector we need
+    cart => cart.cartItems
+);
+
+// output selectors onUpdate from cart items
+export const selectCartItemsCount = createSelector(
+    [selectCartItems],
+    cartItems =>
+        cartItems.reduce(
+            (accumulatedQuantity, cartItems) => accumulatedQuantity + cartItems.quantity, 0
+        )
+);
